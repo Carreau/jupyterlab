47c206ebedc03d05c559eca046d9362850b55bc0
M Bussonnier <bussonniermatthias@gmail.com>
2026-04-01 13:31:07 +0200

    -vvvv

--- scripts/ci_install.sh
+++ scripts/ci_install.sh
@@ -59,7 +59,7 @@ fi
 # Install and enable the server extension
 # Show a verbose install if the install fails, for debugging
 START=$(date +%s)
-uv pip install --system -e "${SPEC}" || uv pip install --system -v -e "${SPEC}"
+pip install --system -vvv -e "${SPEC}"
 step_time $START "uv pip install -e ${SPEC}"
 
 START=$(date +%s)
